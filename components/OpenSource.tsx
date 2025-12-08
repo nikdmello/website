2fa1aa1f41a3cea7b1a96c78e4c22fbb3eb68e6b
Nik <nik.dmello@gmail.com>
2025-12-08 14:13:06 -0600

    fix: remove unused import

--- components/OpenSource.tsx
+++ components/OpenSource.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { Github, GitPullRequest } from 'lucide-react'
+import { Github } from 'lucide-react'
 import Image from 'next/image'
 
 const contributions = [
